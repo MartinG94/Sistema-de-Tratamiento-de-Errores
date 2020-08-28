47c808871551c19e29d18478328d13020dfa6c5c
MartinG94 <guillenmartin94@gmail.com>
2020-08-28 18:10:38 -0300

    Funcionalidad para cerrar la aplicación

--- App/AppClase/Formularios/Frm_Escritorio.cs
+++ App/AppClase/Formularios/Frm_Escritorio.cs
@@ -16,5 +16,10 @@ namespace AppClase
         {
             InitializeComponent();
         }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
