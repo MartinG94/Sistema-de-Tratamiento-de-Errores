ba50bc85dbbe6c3304ecdcb873fcbaac32f86d1f
MartinG94 <guillenmartin94@gmail.com>
2020-08-28 18:18:13 -0300

    Nuevo formulario de Login

--- App/AppClase/Frm_Login.Designer.cs
+++ App/AppClase/Frm_Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AppClase.Formularios
+﻿namespace AppClase
 {
     partial class Frm_Login
     {
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(470, 169);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_Login";
             this.Text = "Frm_Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- App/AppClase/Frm_Login.cs
+++ App/AppClase/Frm_Login.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AppClase.Formularios
+namespace AppClase
 {
     public partial class Frm_Login : Form
     {
