fddfae1c7c3403a743b430a15634418de7ec0e37
MartinG94 <guillenmartin94@gmail.com>
2020-08-28 18:09:31 -0300

    Menú de Archivo y opción Salir

--- App/AppClase/Formularios/Frm_Escritorio.cs
+++ App/AppClase/Formularios/Frm_Escritorio.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AppClase
 {
-    public partial class Form1 : Form
+    public partial class Frm_Escritorio : Form
     {
-        public Form1()
+        public Frm_Escritorio()
         {
             InitializeComponent();
         }
